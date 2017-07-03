56b50c1be332ffcc91e17193f40125ff3db0cdde
rmcintosh <rmcintosh@users.noreply.github.com>
2017-07-03 16:07:59 -0400

    Initial import

---
+++ app.py
@@ -0,0 +1,19 @@
+import json
+from flask import Flask, request, jsonify, render_template
+from flask_limiter.util import get_remote_address
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_ip():
+    format = request.args.get('format', None)
+    ip = get_remote_address()
+    if format == 'json':
+        ip = jsonify(ip=ip)
+    return ip
+
+
+if __name__ == '__main__':
+    app.run()
