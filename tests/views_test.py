38683255077689a1e0b5d840db391528d9577804
rmcintosh <rmcintosh@linode.com>
2017-07-27 14:26:58 -0400

    Add tests

---
+++ tests/views_test.py
@@ -0,0 +1,34 @@
+import unittest
+import sys
+import os
+from flask import jsonify
+sys.path.append(os.path.join(os.getcwd(), '..'))
+
+
+from ipy import app
+
+
+class ViewsTest(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_get_ip(self):
+        """GET /
+        """
+        self.result = self.app.get("/")
+        self.result = self.result.data.decode()
+        self.assertEqual(self.result, "127.0.0.1")
+
+    def test_get_ip_json(self):
+        """GET /?format=json
+        """
+        with app.test_request_context():
+            self.result = self.app.get("/?format=json")
+            self.result = self.result.data.decode()
+            self.expected = jsonify(ip="127.0.0.1").data.decode()
+            self.assertEqual(self.result, self.expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
